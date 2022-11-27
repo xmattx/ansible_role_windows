6746e62cae2ff58e22298c57d5142bde91f9771d
xmattx <70489112+xmattx@users.noreply.github.com>
2022-11-27 12:31:06 +0100

    Add files via upload
    first commit

---
+++ ansiblerole.py
@@ -0,0 +1,98 @@
+import base64, subprocess, argparse, os
+from jinja2 import Template
+
+# STATIC VARS
+META = '''galaxy_info:
+  role_name: {{ role_name }}
+  author: author
+  description: blank
+  platforms:
+    - name: Windows
+      versions:
+        - all
+  license: "GPT-2.0"
+  min_ansible_version: "2.1"
+'''
+
+# ACTUAL CLASS
+class ansibleRole():
+
+    # INIT
+    def __init__(self, roleName=None, command=None, script_dir=None):
+        self.roleName = roleName
+        self.command = command
+
+    # META FILE TEMPLATING FUNCTION
+    def jtemp(self, roleName):
+        
+        path_meta = f"{ roleName }/roles/{roleName}/meta/main.yml"
+        tem_meta = Template(META).render(role_name=roleName)
+
+        with open(path_meta, "w") as f:
+            f.write(tem_meta)
+
+
+    # CMD ENCODER FUNCTION
+    def encodeCommand(self, command):
+        if not isinstance(command, str):
+            exit()
+
+        else:
+            _common_args = ['PowerShell', '-NoProfile', '-NonInteractive', '-ExecutionPolicy', 'Unrestricted']
+            command = command + "| Out-Null"
+            command = command.encode('utf-16-le')
+            command = bytes.decode(base64.b64encode(command))
+            
+            toExecute = _common_args + ['-EncodedCommand', command]
+
+            return toExecute
+
+
+    # ROLE STRUCTURE CREATION
+    def createRoleStructure(self, roleName):
+        commands = [f"New-Item -ItemType Directory -Name { roleName }/collections",
+                    f"New-Item -Name { roleName }/collections/requirements.yml -Value '---' ",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }/defaults",
+                    f"New-Item -name { roleName }/roles/{ roleName }/defaults/main.yml -Value '---' ",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }/files",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }/handlers",
+                    f"New-Item -Name { roleName }/roles/{ roleName }/handlers/main.yml -Value '---' ",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }/meta",
+                    f"New-Item -Name { roleName }/roles/{ roleName }/meta/main.yml -Value '---' ",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }/tasks",
+                    f"New-Item -Name { roleName }/roles/{ roleName }/tasks/main.yml -Value '---' ",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }/templates",
+                    f"New-Item -ItemType Directory -Name { roleName }/roles/{ roleName }/vars",
+                    f"New-Item -Name { roleName }/roles/{ roleName }/vars/main.yml -Value '---' ",
+                    f"New-Item -Name { roleName }/playbook.yml -Value '---' ",
+                    f"New-Item -Name { roleName }/inventory.ini -Value '---' ",
+                    f"new-item -Name { roleName }/README.md -Value '# { roleName }'"]
+        
+
+
+        for command in commands:
+            psCommand = self.encodeCommand(command)
+            subprocess.run(psCommand)
+
+        # with open(f"{ roleName }/roles/{ roleName }/meta/main.yml", "w") as meta:    
+        #     meta.write(META)
+            
+        self.jtemp(roleName)
+
+# PARSING ARGS
+parser = argparse.ArgumentParser()
+parser.add_argument('--roleName', dest='roleName', type=str, help='Role name', required=True)
+_args = parser.parse_args()
+roleName = _args.roleName
+
+# RUNNING PROGRAM WITH PASSED ROLENAME
+if __name__ == "__main__":
+
+    # CHECK IF PATH ALREADY EXISTS
+    if os.path.exists(roleName):
+        print(f"A role named { roleName } already exists!")
+        exit(1)
+
+    creator = ansibleRole()
+    creator.createRoleStructure(roleName)
